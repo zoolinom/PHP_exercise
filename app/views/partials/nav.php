8f3fedb65c18e3488f05ca60845ee0d2b8dd8635
Zoki <zoolinom@gmail.com>
2017-11-09 21:52:39 +0100

    Removed unnecessary id's and added echo of class="active"

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -9,9 +9,9 @@
 
 <nav>
     <ul>
-        <li><a id="tab1" class="<?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == '') echo 'active' ?>" href="/">Home</a></li>
-        <li><a id="tab2" class="<?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == 'patients') echo 'active' ?>" href="/patients">Patients</a></li>
-        <li><a id="tab3" class="<?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == 'doctors') echo 'active' ?>" href="/doctors">Doctors</a></li>
-        <li><a id="tab4" class="<?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == 'records') echo 'active' ?>" href="/records">Records</a></li>
+        <li><a <?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == '') echo 'class="active"'; ?> href="/">Home</a></li>
+        <li><a <?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == 'patients') echo 'class="active"'; ?> href="/patients">Patients</a></li>
+        <li><a <?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == 'doctors') echo 'class="active"'; ?> href="/doctors">Doctors</a></li>
+        <li><a <?php if (trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') == 'records') echo 'class="active"'; ?> href="/records">Records</a></li>
     </ul>
 </nav>
